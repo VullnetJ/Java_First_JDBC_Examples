0e7208ce86b7af13442b6a28fc89b99ce017a0ba
VullnetJ <46821353+VullnetJ@users.noreply.github.com>
2022-02-04 10:38:34 +0200

    Update ConnectionParameters.java

--- src/data_access/ConnectionParameters.java
+++ src/data_access/ConnectionParameters.java
@@ -1,16 +1,5 @@
 package data_access;
 
-/**
- * ConnectionParameters.java - Reusable DBMS connection configuration parameters
- * 
- * Here we can set the connection string, user name, and password to connect the
- * desired database. In addition, the vendor-specific error code for primary key
- * violation is defined here. NB! In the real life, this type of information is
- * stored in external configuration files, not as hard-coded values in the
- * program code :-)
- * 
-
-
 public class ConnectionParameters {
 
 	// We can set the user name and password here (in SQLite these are optional)
